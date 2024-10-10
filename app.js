bba204ebdb6709f5243f088de51e0feb726238fa
ClementDaguenet <clement.dgt72@gmail.com>
2024-10-10 16:49:47 +0200

    Start BDD

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
 const localStorage = require("localStorage");
 
 const sqlite3 = require("sqlite3").verbose();
@@ -27,11 +26,11 @@ console.log("Succès de connexion à la BDD");
 // admin -> accède à tout | secretariat -> accede aux fonctions de mail | dev -> accede aux fonctions de mdp | hacker -> accede a tout sauf mail
 
 const port = 61159;
+var userRole = "dev";
 const secretKey = crypto.randomBytes(32).toString("hex");
 app.use(express.json());
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
   const token = localStorage.getItem("authToken");
   if (token == null) return res.sendStatus(401);
   jwt.verify(token, secretKey, (err, user) => {
@@ -53,37 +52,39 @@ app.get("/users", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-  const users = {
-    user1: { password: bcrypt.hashSync("password123", 10) },
-  };
   const { username, password } = req.query;
-  const user = users[username];
-  if (user == null) {
-    return res.status(400).json({ message: "Utilisateur introuvable" });
-  }
-  try {
-    if (await bcrypt.compare(password, user.password)) {
-      const token = jwt.sign({ user: username }, secretKey, {
-        expiresIn: "3h",
-      });
-      res.json({ token });
-      localStorage.removeItem("authToken");
-      localStorage.setItem("authToken", token);
-      console.log(token);
-    } else {
-      res.status(401).json({ message: "Mot de passe incorrect" });
+  const sql = `SELECT * FROM users WHERE email = ?`;
+  db.get(sql, [username], (err, user) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).json({ message: "Erreur d'authentification" });
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Erreur lors de l'authentification" });
-  }
+    if (!user || user.password !== password) {
+      return res.status(401).json({ message: "Identifiants incorrects" });
+    }
+    const token = jwt.sign({ user: username }, secretKey, { expiresIn: "3h" });
+    userRole = user.role;
+    res.json({ token });
+    localStorage.removeItem("authToken");
+    localStorage.setItem("authToken", token);
+    console.log(`L'utilisateur ${user.email} vient de se connecter, son rôle est ${user.role}.`)
+  });
 });
 
-const mailCheckerRouter = require("./routes/mailChecker");
-app.use("/", authenticateToken, mailCheckerRouter);
+// checker le userRole dans la route sinon roles useless
+if (userRole === "admin" || userRole === "secretariat") {
+  const mailCheckerRouter = require("./routes/mailChecker");
+  app.use("/", authenticateToken, mailCheckerRouter);
+} else {
+  console.log("Vous n'êtes pas autorisés à utiliser cette fonctionnalité !");
+}
 
-const mailSpammerRouter = require("./routes/mailSpammer");
-app.use("/", mailSpammerRouter);
+if (userRole === "admin" || userRole === "secretariat") {
+  const mailSpammerRouter = require("./routes/mailSpammer");
+  app.use("/", authenticateToken, mailSpammerRouter);
+} else {
+  console.log("Vous n'êtes pas autorisés à utiliser cette fonctionnalité !");
+}
 
 app.listen(port, () => {
   console.log(`Serveur en écoute sur le port ${port}`);
